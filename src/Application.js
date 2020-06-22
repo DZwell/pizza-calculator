67df0bb1c669caa5d818024d88bc9879926a5309
Daniel Zwelling <daniel.zwelling@milliman.com>
2020-06-21 21:52:49 -0700

    Implement HOC pattern

--- src/Application.js
+++ src/Application.js
@@ -1,41 +1,15 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 
 import { PizzaCalculator } from './PizzaCalculator';
-import calculatePizzasNeeded from './lib/calculate-pizzas-needed';
+import { WithPizzaCalculations } from './PizzaHOC';
 
-const initialState = {
-  numberOfPeople: 10,
-  slicesPerPerson: 2,
-};
-
-export default class Application extends Component {
-  state = { ...initialState };
-
-  updateNumberOfPeople = event => {
-    const numberOfPeople = parseInt(event.target.value, 10);
-    this.setState({ numberOfPeople });
-  };
-
-  updateSlicesPerPerson = event => {
-    const slicesPerPerson = parseInt(event.target.value, 10);
-    this.setState({ slicesPerPerson });
-  };
-
-  reset = event => {
-    this.setState({ ...initialState });
-  };
+export default class Application extends React.Component {
+  PizzaContainer = WithPizzaCalculations(PizzaCalculator);
 
   render() {
-    const { numberOfPeople, slicesPerPerson } = this.state;
-    const numberOfPizzas = calculatePizzasNeeded(
-      numberOfPeople,
-      slicesPerPerson,
-    );
-    const props = { numberOfPeople, numberOfPizzas, reset: this.reset, slicesPerPerson, updateNumberOfPeople: this.updateNumberOfPeople, updateSlicesPerPerson: this.updateSlicesPerPerson };
-
     return (
       <div className="Application">
-        <PizzaCalculator {...props} />
+        <this.PizzaContainer />
       </div>
     );
   }
---
+++ src/PizzaHOC.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import calculatePizzasNeeded from './lib/calculate-pizzas-needed';
+
+const initialState = {
+  numberOfPeople: 10,
+  slicesPerPerson: 2,
+};
+
+export const WithPizzaCalculations = WrappedComponent => class extends Component {
+  state = { ...initialState };
+
+  updateNumberOfPeople = event => {
+    const numberOfPeople = parseInt(event.target.value, 10);
+    this.setState({ numberOfPeople });
+  };
+
+  updateSlicesPerPerson = event => {
+    const slicesPerPerson = parseInt(event.target.value, 10);
+    this.setState({ slicesPerPerson });
+  };
+
+  reset = event => {
+    this.setState({ ...initialState });
+  };
+
+  render() {
+    const { numberOfPeople, slicesPerPerson } = this.state;
+    const numberOfPizzas = calculatePizzasNeeded(
+      numberOfPeople,
+      slicesPerPerson,
+    );
+    const props = {
+      numberOfPeople,
+      numberOfPizzas,
+      reset: this.reset,
+      slicesPerPerson,
+      updateNumberOfPeople: this.updateNumberOfPeople,
+      updateSlicesPerPerson: this.updateSlicesPerPerson
+    };
+
+    return <WrappedComponent {...props} />
+  }
+}
